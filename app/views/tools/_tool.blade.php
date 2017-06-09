41821cdcb9687d16e88f98a31b1016caf5121c8c
yoann <yoann.moranville@gmail.com>
2017-06-09 09:16:34 +0200

    Add again a warning on the tools page when all information have not been completed for the tool.

--- app/views/tools/_tool.blade.php
+++ app/views/tools/_tool.blade.php
@@ -8,7 +8,11 @@
         <!-- /small-3.columns -->
 
         <div class="small-9 columns">
+            @if (!$tool->is_filled)
+            <h1 itemprop="name"><a href="{{ URL::route("tools.show", $tool->slug) }}" title="All the information are not filled" class="red-warning">{{{ $tool->name }}}</a></h1>
+            @else
             <h1 itemprop="name"><a href="{{ URL::route("tools.show", $tool->slug) }}" title="{{{ $tool->name }}}">{{{ $tool->name }}}</a></h1>
+            @endif
 
             <p>about {{{ BaseHelper::diffForHumans($tool->updated_at) }}}</p>
         </div>
