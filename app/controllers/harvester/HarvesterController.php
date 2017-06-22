864c2c1062ed4a020539eff9b91d48c11b375b62
yoann <yoann.moranville@gmail.com>
2017-06-22 09:29:22 +0200

    Continue the harvester

--- app/controllers/harvester/HarvesterController.php
+++ app/controllers/harvester/HarvesterController.php
@@ -1,23 +1,31 @@
 <?php namespace Harvester;
 
 use BaseController;
+use Data;
+use DataSource;
+use DateTime;
 use Goutte\Client;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
+use Services\DataService;
 use Services\DataTypeService;
 use Services\ToolService;
 use Symfony\Component\DomCrawler\Crawler;
+use Tool;
 
 class HarvesterController extends BaseController
 {
     protected $toolService;
+    protected $dataService;
     protected $dataTypeService;
 
-    public function __construct(ToolService $toolService, DataTypeService $dataTypeService)
+    public function __construct(ToolService $toolService, DataService $dataService, DataTypeService $dataTypeService)
     {
         parent::__construct();
         $this->toolService = $toolService;
+        $this->dataService = $dataService;
         $this->dataTypeService = $dataTypeService;
     }
 
@@ -45,30 +53,60 @@ class HarvesterController extends BaseController
     {
         $url = Input::get("url");
         Log::info("The URL to crawl: " . $url);
+        $dataTypes = $this->dataTypeService->all()->get();
+
+        $dataSources = DataSource::all();
+        foreach($dataSources as $dataSource) {
+            if($dataSource->slug == "has-tool-registry") {
+                $sourceId = $dataSource->id;
+            }
+        }
 
         $client = new Client();
         $crawler = $client->request('GET', urldecode($url));
-        $crawler->filter('article[itemtype="http://schema.org/SoftwareApplication"]')->each(function (Crawler $node) {
-            $dataTypes = $this->dataTypeService->all()->get();
+        $crawler->filter('article[itemtype="http://schema.org/SoftwareApplication"]')->each(function (Crawler $node) use($dataTypes, $sourceId) {
+            $selectedTool = $node->filter('*[itemprop="name"]')->each(function (Crawler $subNode) {
+                Log::info('Create tool with name: --> ' . $subNode->text());
 
-            $node->filter('*[property="http://purl.org/dc/terms/title"]')->each(function (Crawler $subNode) {
-                Log::info('http://purl.org/dc/terms/title --> ' . $subNode->text());
+                $toolFound = $this->toolService->findByName($subNode->text());
+                if(! $toolFound) {
+                    $this->toolService->create($this->inputWithAuthenticatedUserId(array("name" => $subNode->text())));
+                    $toolFound = $this->toolService->findByName($subNode->text());
+                }
+                Log::info("Tool found: " . $toolFound->id);
+                return $toolFound;
             });
-            foreach($dataTypes as $dataType) {
-                $node->filter('*[property="' . $dataType->rdf_mapping . '"]')->each(function (Crawler $subNode) use($dataType) {
-                    Log::info($dataType->rdf_mapping . ' --> ' . $subNode->text());
-                });
+            if($selectedTool) {
+                Log::info("We have a tool on this page, it contains at least a name");
+                $myGoodTool = $selectedTool[0];
+                $this->toolService->attachDataSource($myGoodTool->id, $sourceId);
+                foreach ($dataTypes as $dataType) {
+                    $node->filter('*[property="' . $dataType->rdf_mapping . '"]')->each(function (Crawler $subNode) use ($dataType, $myGoodTool, $sourceId) {
+                        Log::info($dataType->rdf_mapping . ' --> ' . $subNode->text());
+
+                        $dataFound = $this->dataService->findByValue($subNode->text());
+                        if(! $dataFound) {
+                            $this->dataService->create($this->inputWithAuthenticatedUserId(array("name" => $subNode->text())));
+                            $d = new Data;
+                            $d->fill([
+                                "value" => $subNode->text(),
+                                "tool_id" => $myGoodTool->id,
+                                "data_type_id" => $dataType->id,
+                                "data_source_id" => $sourceId,
+                                "user_id" => Auth::user()->id,
+                                "created_at" => new DateTime,
+                                "updated_at" => new DateTime
+                            ]);
+                            $d->save();
+                            Log::info($d->id);
+
+                            $dataFound = $this->dataService->findByValue($subNode->text());
+                        }
+                        Log::info("We have this data in the DB: " . $dataFound->id);
+                    });
+                }
             }
         });
-
-
-//        $parameters[] = array();
-//        $parameters["query"] = "postgresql";
-//        $results = $this->toolService->search($parameters);
-//        $tools = $results["tools"];
-//        foreach ($tools as $tool) {
-//            Log::info($tool->name);
-//        }
 
         return View::make("harvester.show");
     }
