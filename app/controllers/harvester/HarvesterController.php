7329fde51ab49066820b6ce5e43dfa7879058f4d
yoann <yoann.moranville@gmail.com>
2017-06-26 12:48:24 +0200

    Switch from microdata to RDFa

--- app/controllers/harvester/HarvesterController.php
+++ app/controllers/harvester/HarvesterController.php
@@ -23,6 +23,8 @@ class HarvesterController extends BaseController
     protected $dataService;
     protected $dataTypeService;
 
+    private static $vocabSoftwareApplication = "http://schema.org/SoftwareApplication";
+
     public function __construct(ToolService $toolService, DataService $dataService, DataTypeService $dataTypeService)
     {
         parent::__construct();
@@ -67,60 +69,63 @@ class HarvesterController extends BaseController
         $client = new Client();
         $crawler = $client->request('GET', urldecode($url));
         $crawler->filter('article[vocab="http://schema.org/"][typeof="SoftwareApplication"]')->each(function (Crawler $node) use($dataTypes, $sourceId) {
-            Log::info("Passed it");
-            $selectedTool = $node->filter("*[property='http://purl.org/dc/terms/title']")->each(function (Crawler $subNode) {
-                $toolFound = $this->toolService->findByName($subNode->text());
-                if(! $toolFound) {
-                    $this->toolService->create($this->inputWithAuthenticatedUserId(array("name" => $subNode->text())));
+            $vocab = $node->attr("vocab");
+            $typeof = $node->attr("typeof");
+            if(($vocab.$typeof) == static::$vocabSoftwareApplication) {
+                $selectedTool = $node->filter("*[property='http://purl.org/dc/terms/title']")->each(function (Crawler $subNode) {
                     $toolFound = $this->toolService->findByName($subNode->text());
-                }
-                return $toolFound;
-            });
-            if($selectedTool) {
-                Log::info("We have a tool on this page, it contains at least a name");
-                $myTool = $selectedTool[0];
-                $this->toolService->attachDataSource($myTool->id, $sourceId);
-                foreach ($dataTypes as $dataType) {
-                    $rdfFullUri = $dataType->rdf_mapping;
-                    if(! Str::startsWith($rdfFullUri, "http://")) {
-                        $rdfFullUri = "http://schema.org/" . $rdfFullUri;
+                    if (!$toolFound) {
+                        $this->toolService->create($this->inputWithAuthenticatedUserId(array("name" => $subNode->text())));
+                        $toolFound = $this->toolService->findByName($subNode->text());
                     }
-                    $node->filter('*[property="' . $rdfFullUri . '"]')->each(function (Crawler $subNode) use ($dataType, $myTool, $sourceId, $rdfFullUri) {
-                        Log::info($rdfFullUri . ' --> ' . $subNode->text());
-
-                        $dataFound = $this->dataService->findByValueAndTool($myTool->id, $subNode->text());
-                        if(! $dataFound) {
-                            $correctWithDataTypeOption = false;
-                            if($dataType->dataTypeOption()->count() > 0) {
-                                foreach ($dataType->dataTypeOption()->get() as $dataTypeOption) {
-                                    if ($subNode->text() == $dataTypeOption->value) {
-                                        $correctWithDataTypeOption = true;
-                                    }
-                                }
-                            } else {
-                                $correctWithDataTypeOption = true;
-                            }
-                            if($correctWithDataTypeOption) {
-                                $d = new Data;
-                                $d->fill([
-                                    "value" => $subNode->text(),
-                                    "tool_id" => $myTool->id,
-                                    "data_type_id" => $dataType->id,
-                                    "data_source_id" => $sourceId,
-                                    "user_id" => Auth::user()->id,
-                                    "created_at" => new DateTime,
-                                    "updated_at" => new DateTime
-                                ]);
-                                $d->save();
-                                Log::info("Saved data id:" . $d->id);
-//                                $dataFound = $this->dataService->find($d->id);
-                            }
+                    return $toolFound;
+                });
+                if ($selectedTool) {
+                    Log::info("We have a tool on this page, it contains at least a name");
+                    $myTool = $selectedTool[0];
+                    $this->toolService->attachDataSource($myTool->id, $sourceId);
+                    foreach ($dataTypes as $dataType) {
+                        $rdfFullUri = $dataType->rdf_mapping;
+                        if (!Str::startsWith($rdfFullUri, $vocab)) {
+                            $rdfFullUri = $vocab . $rdfFullUri;
                         }
-                    });
-                }
-                if($myTool->isFilledSingle()) {
-                    $myTool->is_filled = true;
-                    $myTool->save();
+                        $node->filter('*[property="' . $rdfFullUri . '"]')->each(function (Crawler $subNode) use ($dataType, $myTool, $sourceId, $rdfFullUri) {
+                            Log::info($rdfFullUri . ' --> ' . $subNode->text());
+
+                            $dataFound = $this->dataService->findByValueAndTool($myTool->id, $subNode->text());
+                            if (!$dataFound) {
+                                $correctWithDataTypeOption = false;
+                                if ($dataType->dataTypeOption()->count() > 0) {
+                                    foreach ($dataType->dataTypeOption()->get() as $dataTypeOption) {
+                                        if ($subNode->text() == $dataTypeOption->value) {
+                                            $correctWithDataTypeOption = true;
+                                        }
+                                    }
+                                } else {
+                                    $correctWithDataTypeOption = true;
+                                }
+                                if ($correctWithDataTypeOption) {
+                                    $d = new Data;
+                                    $d->fill([
+                                        "value" => $subNode->text(),
+                                        "tool_id" => $myTool->id,
+                                        "data_type_id" => $dataType->id,
+                                        "data_source_id" => $sourceId,
+                                        "user_id" => Auth::user()->id,
+                                        "created_at" => new DateTime,
+                                        "updated_at" => new DateTime
+                                    ]);
+                                    $d->save();
+                                    Log::info("Saved data id:" . $d->id);
+                                    //                                $dataFound = $this->dataService->find($d->id);
+                                }
+                            }
+                        });
+                    }
+                    if ($myTool->isFilledSingle()) {
+                        $myTool->is_filled = true;
+                        $myTool->save();
+                    }
                 }
             }
         });
