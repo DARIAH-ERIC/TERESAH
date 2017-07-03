543d2d8813b80919d70fcc5194faa8b02a31169b
yoann <yoann.moranville@gmail.com>
2017-07-03 12:54:06 +0200

    Revert

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -30,9 +30,8 @@ if (strtoupper(substr(PHP_OS, 0, 3)) === "WIN") {
     });
 } else {
     $env = $app->detectEnvironment(array(
-        "development" => array("*.local", "*.localhost"),
-        "staging" => array("dasish"),
-        "production" => array("teresah")
+        "development" => array("teresah", "*.local", "*.localhost"),
+        "staging" => array("dasish")
     ));
 }
 
