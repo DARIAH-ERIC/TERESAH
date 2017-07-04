d0af251becd69d3e8fbc7274f31ba27f7c82badd
yoann <yoann.moranville@gmail.com>
2017-07-04 11:12:38 +0200

    No edition on the fly because of the different editing choices we now have

--- app/views/admin/tools/data_sources/data/_data.blade.php
+++ app/views/admin/tools/data_sources/data/_data.blade.php
@@ -1,7 +1,7 @@
 @if ($data->dataType)
     <tr>
         <td>{{{ $data->dataType->label }}}</td>
-        <td>{{ link_to_route("admin.tools.data-sources.data.update", e($data->value), array($tool->id, $dataSource->id, $data->id), array("class" => "editable", "data-pk" => $data->id, "data-name" => "value", "data-url" => URL::route("admin.tools.data-sources.data.update", array($tool->id, $dataSource->id, $data->id)), "data-type" => "text")) }}</td>
+        <td>{{ $data->value }}</td>
         <td>{{ link_to_route("admin.users.show", e($data->user->name), array("id" => $data->user->id), array("title" => e($data->user->name))) }}</td>
         <td>
             <a href="{{ URL::route("admin.tools.data-sources.data.edit", array($tool->id, $dataSource->id, $data->id)) }}" title="{{ Lang::get("views.admin.tools.data_sources.show.actions.edit.title") }}"><span class="glyphicons pencil"></span></a> 
