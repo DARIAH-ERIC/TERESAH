84561545c2c7461ddad1f089832e802d7974a89d
yoann <yoann.moranville@gmail.com>
2017-10-13 11:39:55 +0200

    Modify descriptions to be the same as in the D8.1

--- app/database/seeds/DataTypesTableSeeder.php
+++ app/database/seeds/DataTypesTableSeeder.php
@@ -8,57 +8,57 @@ class DataTypesTableSeeder extends Seeder
         $dataTypes = array(
             array("label" => "Description", 
                   "rdf_mapping" => "http://schema.org/description",
-                  "description" => "General description of the tool",
+                  "description" => "A description of the item",
                   "linkable" => false
                  ),
             array("label" => "Url",
                   "rdf_mapping" => "http://schema.org/url", 
-                  "description" => "URL to the tool homepage",                
+                  "description" => "URL of the item",
                   "linkable" => false
                  ),
             array("label" => "Creator",
                   "rdf_mapping" => "http://schema.org/creator",
-                  "description" => "Organization or person who developed the tool"
+                  "description" => "The creator/author of this CreativeWork. This is the same as the Author property for CreativeWork"
                  ),
             array("label" => "Keyword", 
                   "rdf_mapping" => "http://schema.org/keywords",
-                  "description" => "Free form keywords describing the tool"
+                  "description" => "Keywords describing an item"
                  ),
             array("label" => "License", 
                   "rdf_mapping" => "http://schema.org/license",
-                  "description" => "Type of licence for the tool"
+                  "description" => "A license document that applies to this content, typically indicated by URL"
                  ),
             array("label" => "Operating System",
                   "rdf_mapping" => "http://schema.org/operatingSystem",
-                  "description" => "Platform the tool runs on"
+                  "description" => "Operating systems supported (Windows 7, OSX 10.6, Android 1.6)"
                  ),
             array("label" => "Standard", 
                   "rdf_mapping" => "http://schema.org/supportingData",
-                  "description" => "Supported standard for the tool"
+                  "description" => "Standards used in this item"
             ),
             array("label" => "Type",
                   "rdf_mapping" => "http://purl.org/dc/elements/1.1/type",
-                  "description" => "General type"
+                  "description" => "The nature or genre of the resource"
                  ),
             array("label" => "Contributor",
                 "rdf_mapping" => "http://schema.org/contributor",
-                "description" => "Organization or person who contributed to the tool"
+                "description" => "A secondary contributor to the CreativeWork or Event"
             ),
             array("label" => "Application Category",
                 "rdf_mapping" => "http://schema.org/applicationCategory",
-                "description" => "Type of software application, e.g. 'Game Multimedia'"
+                "description" => "Type of software application, e.g. 'Game, Multimedia'"
             ),
             array("label" => "Service Type",
                 "rdf_mapping" => "http://schema.org/serviceType",
-                "description" => "Type of service being offered, e.g. 'veteran's benefits', 'emergency relief', etc..."
+                "description" => "The type of service being offered, e.g. veterans' benefits, emergency relief, etc"
             ),
             array("label" => "Is Used For",
                 "rdf_mapping" => "http://purl.org/dc/elements/1.1/subject",
-                "description" => "TaDiRAH Research Activities describes how tools and services can be used for."
+                "description" => "TaDiRAH Research Techniques describes what the tools and services can be used for"
             ),
             array("label" => "Research Object",
                 "rdf_mapping" => "http://schema.org/object",
-                "description" => "The object upon which the action is carried out. Used with the ontology of TaDiRAH Research Object."
+                "description" => "The object upon which the action is carried out. Used with the ontology of TaDiRAH Research Object"
             ),
             array("label" => "Date Created",
                 "rdf_mapping" => "http://schema.org/dateCreated",
@@ -80,15 +80,15 @@ class DataTypesTableSeeder extends Seeder
             ),
             array("label" => "Processor Requirements",
                 "rdf_mapping" => "http://schema.org/processorRequirements",
-                "description" => "Processor architecture required to run the application, e.g. 'IA64'"
+                "description" => "Processor architecture required to run the application (e.g. IA64)"
             ),
             array("label" => "Software Requirements",
                 "rdf_mapping" => "http://schema.org/softwareRequirements",
-                "description" => "Component dependency requirements for the application. This includes runtime environments and shared libraries that are not included in the application distribution package, but required to run the application, e.g. 'DirectX', 'Java' or '.NET runtime'"
+                "description" => "Component dependency requirements for application. This includes runtime environments and shared libraries that are not included in the application distribution package, but required to run the application (Examples: DirectX, Java or .NET runtime)"
             ),
             array("label" => "Browser Requirements",
                 "rdf_mapping" => "http://schema.org/browserRequirements",
-                "description" => "Specifies browser requirements in human-readable text, e.g. 'Requires HTML5 support'"
+                "description" => "Specifies browser requirements in human-readable text. For example, 'requires HTML5 support'"
             ),
             array("label" => "Storage Requirements",
                 "rdf_mapping" => "http://schema.org/storageRequirements",
