0da77e1924e363b1cbf516c0ec4ad91cb240bb6c
yoann <yoann.moranville@gmail.com>
2018-07-05 12:18:34 +0200

    Add google analytics

--- app/views/shared/_head.blade.php
+++ app/views/shared/_head.blade.php
@@ -66,4 +66,14 @@
                 }
             })});
     </script>
+
+@if(isset($_ENV["GOOGLE_ANALYTICS"]))
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ $_ENV['GOOGLE_ANALYTICS'] }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '{{ $_ENV["GOOGLE_ANALYTICS"] }}');
+    </script>
+@endif
 </head>
